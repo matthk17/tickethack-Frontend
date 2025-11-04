61237263d6a93e0abf2f76df524ecc380b8438fb
Your name <Your email>
2025-11-04 17:47:37 +0100

    main page frontend

---
+++ script.js
@@ -0,0 +1,11 @@
+// Boutons:
+    // - Tickethack => Direction page d'acceuil
+    // - Cart => dirtection page cart
+    // - Booking => direction page booking
+// Fonctions:
+    // - Page accueil => Recherche du trajet vers le backend
+    // - Page acceuil => Afficher l'absence de trip ds le backend
+    // - Page cart => Afficher le trip selectionne
+    // - Page cart => Suppression d'un trip
+    // - Page booking => Afficher le trip purchase de la page cart
+    // - Page booking => Enregistrer le trip booking dans la BDD
